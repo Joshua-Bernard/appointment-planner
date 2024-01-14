2ec763d0ed69d3dbd37bf5cedcc69bb58c80693a
Josh <joshualeebernard@gmail.com>
2024-01-14 10:20:09 +0000

    Update contacts and appointments state variables to include a default test entry

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
   Define state variables for 
   contacts and appointments 
   */
-  const [contacts, setContacts] = useState([{ name: "test", phone: "test", email: "test"}]);
-  const [appointments, setAppointments] = useState([]);
+  const [contacts, setContacts] = useState([{ name: "test name", phone: "test phone", email: "test email"}]);
+  const [appointments, setAppointments] = useState([{ title: "test title", contact: "test contact", date: "test date", time: "test time"}]);
   /*
   Implement functions to add data to
   contacts and appointments
