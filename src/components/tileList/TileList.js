7bbfbd6a506a5d5873441115bc360b8b33843106
Josh <joshualeebernard@gmail.com>
2024-01-14 10:22:27 +0000

    Refactor TileList component to destructure tiles and update Tile props

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Tile } from "../tile/Tile";
 
-export const TileList = (props) => {
+export const TileList = ({tiles}) => {
   return (
     <div>
-      {props.objArray.map((obj, index) => (
-        <Tile key={index} name={obj.name} description={obj.description} />
-      ))}
+      {tiles.map((tile, index) => {
+        const {name, ...description } = tile;
+        return <Tile key={index} name={name} description={description}/>
+    })}
     </div>
   );
 };
