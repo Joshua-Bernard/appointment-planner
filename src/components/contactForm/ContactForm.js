c46b29aecc8b9c75d4b125bafa187753e1f9b46a
Josh <joshualeebernard@gmail.com>
2024-01-14 09:05:09 +0000

    Add form and input fields to ContactForm component

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -7,10 +7,38 @@ export const ContactForm = ({
   setPhone,
   email,
   setEmail,
-  handleSubmit
+  handleSubmit,
 }) => {
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Contact Name"
+          required
+        />
+        <input
+          type="tel"
+          name="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          placeholder="Contact Phone"
+          pattern="^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$"
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Contact Email"
+          required
+        />
+        <input type="submit" value="Add Contact" />
+      </form>
+    </>
   );
 };
-
