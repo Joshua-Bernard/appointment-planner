ddedb0fe920f9305ca85c1f0181b5cf08ebba43e
Josh <joshualeebernard@gmail.com>
2024-01-14 11:01:25 +0000

    Refactor ContactPicker component to include dynamic options

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = (props) => {
   return (
-    <></>
+    <>
+      <select
+        value={props.value}
+        name={props.name}
+        onChange={props.onChange}
+        aria-label="Contact Picker"
+      >
+        <option value={""} key={-1}>
+          No Contact Selected
+        </option>
+        {props.contacts.map((contact) => {
+          return (
+            <option value={contact} key={contact}>
+              {contact}
+            </option>
+          );
+        })}
+      </select>
+    </>
   );
 };
