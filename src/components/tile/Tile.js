1778e961a006bd87b0f2397190436e3db18a8de0
Josh <joshualeebernard@gmail.com>
2024-01-14 10:22:49 +0000

    Refactor Tile component to accept props for name and description

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = (props) => {
   return (
     <div className="tile-container">
-      
+      <p className="tile-title">{props.name}</p>
+      {props.description && Object.values(props.description).map((value, index) => (
+        <p key={index} className="tile">
+          {value}
+        </p>
+      ))}
     </div>
   );
 };
